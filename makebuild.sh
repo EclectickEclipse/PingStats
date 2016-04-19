08649701609b4808dafe5cf4fcc6abe7b2325a5c
EclectickEclipse <ariana.giroux@gmail.com>
2016-04-19 01:02:26 -0600

    ./makebuild rewrite to make the code easier to read, and function better. Added functionality for named builds.

--- makebuild.sh
+++ makebuild.sh
@@ -1,110 +1,39 @@
 #!/usr/bin/env bash
-# Requires options BUILDFILE, BUILDNAME, Distribute option
+# Requires options BUILDFILE, BUILDNAME, DISTRIBUTE_FLAG
 # BUILDNAME should be the same as the BUILDFILE, without .py...
 
-# TODO BUG FileCreationError: Upon execution, creates a random blank file titled either 1 or 2. REQUIRES BUG REPORT
+# TODO BUG FileCreationError: The random blank file is actually titled the exit status of the program.
 
-if [ $(cat $1 | grep -c "# TODO") > 1 ]; then
+if [ $(cat $1 | grep -c "# TODO") > 0 ]; then
     echo "There are TODO's that require attention, would you like to review them? [y/n]"
     say "There are TODO's that require your attention!"
     read checkfortodo
     if [ "$checkfortodo" == "y" ]; then
         cat $1 | grep "# TODO"
-        echo "Would you still like to build? [y/n]"
+        echo "Build? [y/n]"
         read checkforbuild
-        if [ "$checkforbuild" == "y" ]; then
-            echo "Building $1!"
-            #echo "Removing old build files..."
-            #rm -r build &> /dev/null
-            #rm -r dist &> /dev/null
-            #rm -r $2.spec &> /dev/null
-
-            # UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-            say "Performing build!"
-            pyinstaller -F -c $1
-
-            if [ "$3" == "dist" ]; then
-                echo "Removing old binary '$2' from /usr/local/bin/ and copying the new one."
-                rm /usr/local/bin/$2
-                cp ./dist/$2 /usr/local/bin/$2
-            fi
-
-            echo "Done!"
-            # UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-            say "Done!"
-            echo ""
-            echo "Testing build..."
-            $2 -v
-        else echo "Exiting!"; say "Exiting!"; exit
+        if [ "$checkforbuild" == "n" ]; then
+            echo "Exiting!"; say "Exiting"; exit
         fi
-        exit
-    else
-        #echo "Removing old build files..."
-        #rm -r build &> /dev/null
-        #rm -r dist &> /dev/null
-        #rm -r $2.spec &> /dev/null
-
-        echo "Performing build..."
-        # UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-        say "Performing build!"
-        pyinstaller -F -c $1
-
-        if [ "$3" == "dist" ]; then
-            echo "Removing old binary '$2' from /usr/local/bin/ and copying the new one."
-            rm /usr/local/bin/$2
-            cp ./dist/$2 /usr/local/bin/$2
-        fi
-        echo "Done!"
-        # UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-        say "Done!"
-        echo ""
-        echo "Testing build..."
-        $2 -v
     fi
-else
-    #echo "Removing old build files..."
-    #rm -r build &> /dev/null
-    #rm -r dist &> /dev/null
-    #rm -r $2.spec &> /dev/null
-
-    echo "Performing build..."
-    # UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-    say "Performing build!"
-    pyinstaller -F -c $1
-
-    if [ "$3" == "dist" ]; then
-        echo "Removing old binary '$2' from /usr/local/bin/ and copying the new one."
-        rm /usr/local/bin/$2
-        cp ./dist/$2 /usr/local/bin/$2
-    fi
-
-    echo "Done!"
-    # UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-    say "Done!"
-    echo ""
-    echo "Testing build..."
-    $2 -v
 fi
 
-#echo "Removing old build files..."
-#rm -r build &> /dev/null
-#rm -r dist &> /dev/null
-#rm -r $2.spec &> /dev/null
+echo "Building $1!"
 
-#echo "Performing build..."
-## UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-#say "Performing build!"
-#pyinstaller -F -c $1
-#
-#if [ "$3" == "dist" ]; then
-#    echo "Removing old binary '$2' from /usr/local/bin/ and copying the new one."
-#    rm /usr/local/bin/$2
-#    cp ./dist/$2 /usr/local/bin/$2
-#fi
-#
-#echo "Done!"
-## UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
-#say "Done!"
-#echo ""
-#echo "Testing build..."
-#$2 -v
+# UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
+say "Performing build!"
+pyinstaller -F -c -n "$2" "$1"
+
+if [ "$3" == "dist" ]; then
+    echo "Removing old binary '$2' from /usr/local/bin/ and copying the new one."
+    rm /usr/local/bin/$2
+    cp ./dist/$2 /usr/local/bin/$2
+fi
+
+echo "Done!"
+# UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
+say "Done!"
+echo ""
+echo "Testing build..."
+./dist/$2 -v
+
