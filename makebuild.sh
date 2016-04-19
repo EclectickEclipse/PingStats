a05ec33ccc6b689a23a3f846112a91cb1754a296
EclectickEclipse <ariana.giroux@gmail.com>
2016-04-19 02:23:29 -0600

    (V0.07.03a PATCH) Documentation update.

--- makebuild.sh
+++ makebuild.sh
@@ -2,11 +2,13 @@
 # Requires options BUILDFILE, BUILDNAME, DISTRIBUTE_FLAG
 # BUILDNAME should be the same as the BUILDFILE, without .py...
 
-# TODO BUG FileCreationError: Bug was caused the statement if [ $(cat $1 | grep -c "# TODO") > 0] was outputting the results of grep to ./0
+# TODO BUG FileCreationError: Bug was caused by the statement "if [ $(cat $1 | grep -c "# TODO") > 0]" was outputting the results of grep to ./0
+# TODO BUG FileCreationError: Review Pull Request #9 for full bug report.
 # TODO BUG FileCreationError: **CLOSED**
 
 if [ "$(cat $1 | grep -c "# TODO")" != "0" ]; then  # logic changed to reflect usage.
     echo "There are TODO's that require attention, would you like to review them? [y/n]"
+    # COMMENT SAY LINES FOR NON MACOSX SYSTEMS
     say "There are TODO's that require your attention!"
     read checkfortodo
     if [ "$checkfortodo" == "y" ]; then
@@ -21,7 +23,7 @@ fi
 
 echo "Building $1!"
 
-# UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
+# COMMENT SAY LINES FOR NON MACOSX SYSTEMS
 say "Performing build!"
 
 pyinstaller -F -c -n "$2" "$1"
@@ -32,10 +34,8 @@ if [ "$3" == "dist" ]; then
     cp ./dist/$2 /usr/local/bin/$2
 fi
 
-#rm ./0 &> /dev/null
-
 echo "Done!"
-# UNCOMMENT SAY LINES FOR NON MACOSX SYSTEMS
+# COMMENT SAY LINES FOR NON MACOSX SYSTEMS
 say "Done!"
 echo ""
 echo "Testing build..."
