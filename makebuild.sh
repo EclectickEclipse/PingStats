8b34168ea8b10b697a08e01517eb8ca23276d20c
EclectickEclipse <ariana.giroux@gmail.com>
2016-04-26 23:36:24 -0600

    V0.08a Documentation update

--- makebuild.sh
+++ makebuild.sh
@@ -2,10 +2,6 @@
 # Requires options BUILDFILE, BUILDNAME, DISTRIBUTE_FLAG
 # BUILDNAME should be the same as the BUILDFILE, without .py...
 
-# TODO BUG FileCreationError: Bug was caused by the statement "if [ $(cat $1 | grep -c "# TODO") > 0]" was outputting the results of grep to ./0
-# TODO BUG FileCreationError: Review Pull Request #9 for full bug report.
-# TODO BUG FileCreationError: **CLOSED**
-
 echo "Updating TODO tags for ./PingStats.py and ./makebuil.sh."
 
 echo "TODO MASTER" &> "TODO.txt"
