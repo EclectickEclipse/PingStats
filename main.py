41b0ad29cd4f59077c88d8b1d6c04452a7a9ee27
EclectickEclipse <ariana.giroux@gmail.com>
2016-12-11 06:05:47 -0600

    Ordered parser arguments alphabetically

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-a', '--address', help='The IP address to ping.')
 
+parser.add_argument('-d', '--delay', help='The interval of time (in seconds'
+                    ') to wait between ping requests.', type=float,
+                    default=0.22)
+
+parser.add_argument('-gi', '--generateimage',
+                    help='Used in conjunction with the -pf option, this '
+                    'option sends a name for a \'*.png\' file to save'
+                    ' to the current working directory.', action='store_true')
+
+parser.add_argument('-n', '--name',
+                    help='Flag this option to use a custom name for the'
+                    ' CSV output file.')
+
 parser.add_argument('-p', '--path',
                     help='The path to output csv files to')
 
@@ -16,17 +29,8 @@ parser.add_argument('-pf', '--plotfile',
                     help='Include the path to a previously generated CSV'
                     'file to generate a plot.')
 
-parser.add_argument('-gi', '--generateimage',
-                    help='Used in conjunction with the -pf option, this '
-                    'option sends a name for a \'*.png\' file to save'
-                    ' to the current working directory.')
-
 parser.add_argument('-q', '--quiet', help='Flag this for quiet operation.',
                     action='store_true')
-
-parser.add_argument('-n', '--name',
-                    help='Flag this option to use a custom name for the'
-                    ' CSV output file.')
 
 parser.add_argument('-s', '--showliveplot',
                     help='Flag this option to display an animated plot of'
@@ -49,10 +53,6 @@ parser.add_argument('-sNF', '--nofile',
                     help='Flag this option to disable outputting ping '
                     'information to a csv  file during live plotting.'
                     ' Helps with memory consumption.', action='store_true')
-
-parser.add_argument('-d', '--delay', help='The interval of time (in seconds'
-                    ') to wait between ping requests.', type=float,
-                    default=0.22)
 
 parser.add_argument('-v', '--version',
                     help='Flag this option to display software version.',
