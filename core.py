a72282cf9373513501195b118c2b22d8f27c9115
EclectickEclipse <ariana.giroux@gmail.com>
2016-12-19 02:08:29 -0600

    Updated to V2.3
    Also removed license from core.py, as it is in LICENSE

--- core.py
+++ core.py
@@ -1,25 +1,3 @@
-# This software is provided under the MIT Open Source Software License:
-#
-# Copyright (c) 2016, Ariana Giroux (Eclectick Media Solutions)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import time
 import socket
 import datetime as dt
@@ -30,8 +8,8 @@ from pythonping import ping as pyping
 
 # GLOBALS
 buildname = 'PingStats'
-version = '2.2'
-versiondate = 'Sun Dec  4 05:03:21 2016'
+version = '2.3'
+versiondate = 'Mon Dec 19 02:07:36 2016'
 versionstr = 'PingStats Version %s (C) Ariana Giroux, Eclectick Media ' \
              'Solutions. circa %s' % (
                  version, versiondate)
