867c8ce4bd28bb93d235f853c307997fd7c84e9e
EclectickEclipse <ariana.giroux@gmail.com>
2017-01-07 04:54:52 -0600

    Added settings callbacks and initial plot code
    Added a callback to each aspect of the settings ui that simply updates the
    global settings dict.
    
    Changed the name of GraphScreen to PlotScreen for more consistency throughout
    the project.
    
    Also added basic logic to render plot.Animate to ui.PlotScreen

--- ui.py
+++ ui.py
@@ -11,6 +11,9 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.switch import Switch
 
+# Core
+import plot
+
 settings = {  # GLOBAL SETTINGS DATA
     'address': 'google.ca',
     'track': {
@@ -38,6 +41,10 @@ class MainSettings(GridLayout):
 
     spacing = [50, 50]
 
+    def report_address(self, isntance, value):
+        global settings
+        settings['address'] = value
+
     def __init__(self, **kwargs):
         super(MainSettings, self).__init__(**kwargs)
         # Address
@@ -45,6 +52,8 @@ class MainSettings(GridLayout):
         address_label = Label(text='Address:')
         address_input = TextInput(text='google.ca', multiline=False,
                                   size_hint_y=0.3)
+        address_input.bind(on_text_validate=self.report_address)
+
         self.address_box.add_widget(address_label)
         self.address_box.add_widget(address_input)
         self.add_widget(self.address_box)
@@ -53,10 +62,17 @@ class MainSettings(GridLayout):
 
 
 class MainModeSelect(GridLayout):
-    # TODO track state of switches
     rows = 2
 
     spacing = [50, 50]
+
+    def report_return_time(self, instance, value):
+        global settings
+        settings['track']['return'] = value
+
+    def report_ttl(self, instance, value):
+        global settings
+        settings['track']['ttl'] = value
 
     def __init__(self, **kwargs):
         super(MainModeSelect, self).__init__(**kwargs)
@@ -64,6 +80,7 @@ class MainModeSelect(GridLayout):
         # return time
         self.add_widget(Label(text='Track return time:'))
         self.return_time = Switch(active=True)
+        self.return_time.bind(active=self.report_return_time)
         self.add_widget(self.return_time)
 
         # average ttl
@@ -82,32 +99,57 @@ class AdvancedSettings(GridLayout):
 
     spacing = [50, 50]
 
+    def report_csv_name(self, instance, value):
+        global settings
+        settings['csv']['name'] = value
+
+    def report_csv_path(self, instance, value):
+        global settings
+        settings['csv']['path'] = value
+
+    def report_generate_file(self, instance, value):
+        global settings
+        settings['csv']['generate'] = value
+
+    def report_core_wait(self, instance, value):
+        global settings
+        settings['core']['wait'] = float(value)
+
+    def report_core_length(self, instance, value):
+        global settings
+        settings['core']['length'] = float(value)
+
     def __init__(self, **kwargs):
         super(AdvancedSettings, self).__init__(**kwargs)
 
         # Custom PingStatsLog.csv output name
         self.add_widget(Label(text='CSV output name:'))
         self.csv_name = TextInput(text='PingStatsLog.csv', multiline=False)
+        self.csv_name.bind(on_text_validate=self.report_csv_name)
         self.add_widget(self.csv_name)
 
         # Custom PingStatsLog.csv output path
         self.add_widget(Label(text='CSV output path:'))
         self.csv_path = TextInput(text='./', multiline=False)
+        self.csv_path.bind(on_text_validate=self.report_csv_path)
         self.add_widget(self.csv_path)
 
         # Omit csv file generation
         self.add_widget(Label(text='CSV Log generation:', size_hint_y=0.3))
         self.generate_file = Switch(active=True, size_hint_y=0.3)
+        self.generate_file.bind(active=self.report_generate_file)
         self.add_widget(self.generate_file)
 
         # Time Between Pings
         self.add_widget(Label(text='Time to wait between ping requests'))
         self.core_wait = TextInput(text='0.22', multiline=False)
+        self.core_wait.bind(on_text_validate=self.report_core_wait)
         self.add_widget(self.core_wait)
 
         # PlotTable length
         self.add_widget(Label(text='Number of results to show on screen'))
         self.core_length = TextInput(text='250')
+        self.core_length.bind(on_text_validate=self.report_core_length)
         self.add_widget(self.core_length)
 
 
@@ -130,13 +172,32 @@ class PaneSelect(Accordion):
         self.select(self.main_settings_pane)
 
 
-class GraphScreen(Screen):
+class PlotScreen(Screen):
+    # TODO Actually implement graph
+    def render_graph(self):
+        global settings
+
+        self.plot = plot.Animate(
+            address=settings['address'],
+            file_path=settings['csv']['path'],
+            file_name=settings['csv']['name'],
+            nofile=settings['csv']['generate'],
+            delay=settings['core']['wait'],
+            table_length=settings['core']['length']
+        )
+
+        self.layout.add_widget(plot, index=0)
+
+    def remove_graph(self):
+        self.remove_widget(plot)
+        self.plot = None
+
     def __init__(self, **kwargs):
-        super(GraphScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        self.button = Button(text='hello')
-        layout.add_widget(self.button)
-        self.add_widget(layout)
+        super(PlotScreen, self).__init__(**kwargs)
+        self.layout = BoxLayout(orientation='vertical')
+        self.button = Button(text='Stop')
+        self.layout.add_widget(self.button)
+        self.add_widget(self.layout)
 
 
 class SettingsScreen(Screen):
@@ -158,22 +219,24 @@ class SettingsScreen(Screen):
 class Root(ScreenManager):
     transition = FadeTransition()
 
-    def change(self, instance):
-        if instance.text.count('Go'):
-            self.current = 'graph'
-        else:
-            self.current = 'settings'
+    def show_plot(self, instance):
+        self.current = 'graph'
+        self.graph.render_graph()
+
+    def show_settings(self, instance):
+        self.current = 'settings'
+        self.graph.remove_graph()
 
     def __init__(self, **kwargs):
         super(Root, self).__init__(**kwargs)
-        settings = SettingsScreen(name='settings')
-        graph = GraphScreen(name='graph')
+        self.settings = SettingsScreen(name='settings')
+        self.graph = PlotScreen(name='graph')
 
-        settings.go.bind(on_press=self.change)
-        graph.button.bind(on_press=self.change)
+        self.settings.go.bind(on_press=self.show_plot)
+        self.graph.button.bind(on_press=self.show_settings)
 
-        self.add_widget(settings)
-        self.add_widget(graph)
+        self.add_widget(self.settings)
+        self.add_widget(self.graph)
         self.current = 'settings'
 
 
